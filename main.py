80f133cddb0a91a1f78bc78629c423128384ca6c
Bartek <bartekandrzejtkaczyk@gmail.com>
2021-04-17 13:11:52 +0200

    nie

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+import requests
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello world!"}
+    return {"message": os.environ['REQUEST_METHOD']}
+
+@app.get("/method")
+def method():
+    return {"method": "GET"}
+
+@app.post("/method")
+def method():
+    return {"method": "POST"}
