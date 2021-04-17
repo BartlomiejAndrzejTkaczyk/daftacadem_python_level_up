7516f0a56389285244f46948498472e7c6e2d8d6
Bartek <bartekandrzejtkaczyk@gmail.com>
2021-04-17 13:31:35 +0200

    return json, not return HTML code

--- main.py
+++ main.py
@@ -6,11 +6,23 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": os.environ['REQUEST_METHOD']}
+    return {"message": "Hello world!"}
 
 @app.get("/method")
 def method():
     return {"method": "GET"}
+
+@app.put("/method")
+def method():
+    return {"method": "PUT"}
+
+@app.options("/method")
+def method():
+    return {"method": "OPTIONS"}
+
+@app.delete("/method")
+def method():
+    return {"method": "DELETE"}
 
 @app.post("/method")
 def method():
--- tests.py
+++ tests.py
@@ -10,3 +10,15 @@ def test_read_main():
     print(response.json())
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world!"}
+
+def test_method():
+    response_get = client.get("/method")
+    response_put = client.put("/method")
+    response_post = client.post("/method")
+    response_options = client.options("/method")
+    response_delete = client.delete("/method")
+    assert response_get.json() == {"method": "GET"}
+    assert response_put.json() == {"method": "PUT"}
+    assert response_post.json() == {"method": "POST"}
+    assert response_options.json() == {"method": "OPTIONS"}
+    assert response_delete.json() == {"method": "DELETE"}
