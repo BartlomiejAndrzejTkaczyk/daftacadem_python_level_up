b05375eb4f275247f7a0f95f84941c8b138e1f45
Bartek <bartekandrzejtkaczyk@gmail.com>
2021-04-18 13:21:49 +0200

    koniec pracy

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 import hashlib
 import datetime
+import json
 
 app = FastAPI()
 app.counter_id = 1
-
+app.patients = []
 
 @app.get("/")
 def root():
-    return {"message": "Hello world!"}
+    return {'message': 'Hello world!'}
 
 @app.get("/method")
 def method():
@@ -46,6 +47,8 @@ def auth(password,password_hash):
     else:
         return JSONResponse(status_code=401)
 
+# ---------------
+
 def addDayToDateYMD(date_str:str, days:int, seperator:str = '-'):
     YMD = [int(ele) for ele in date_str.split(seperator)]
     date = datetime.date(YMD[0],YMD[1],YMD[2])
@@ -56,17 +59,41 @@ class Item(BaseModel):
     name: str
     surname: str
 
+class Patient:
+
+    def __init__(self, name, surname, tday=datetime.date.today()):
+        self.id = app.counter_id
+        app.counter_id+=1
+        self.name = name
+        self.surname = surname
+        self.register_date = tday
+        self.vaccination_date = addDayToDateYMD(str(tday),len(self.name + self.surname))
+    
+    def dataInDict(self):
+        return {
+        "id": self.id,
+        "name": self.name,
+        "surname": self.surname,
+        "register_date": str(self.register_date),
+        "vaccination_date": str(self.vaccination_date)
+        }
+    
+    def __str__(self):
+        ans = [ele for ele in self.dataInDict().values()]
+        return str(ans)
 
 @app.post("/register")
 async def register(item: Item):
-    vaccination_date = addDayToDateYMD("2021-04-01",len(item.name + item.surname))
-    tday = datetime.date().today()
-    content={
-        "id": app.counter_id,
-        "name": item.name,
-        "surname": item.surname,
-        "register_date": str(tday),
-        "vaccination_date": str(vaccination_date)
-    }
-    app.counter_id+=1
-    return JSONResponse(content=content,status_code=201)
+    patient = Patient(item.name,item.surname)
+    app.patients.append(patient)
+    return JSONResponse(content=patient.dataInDict(),status_code=201)
+
+@app.get("/patient/{id}")
+def get_patient_id(id: int):
+    if id<1: return JSONResponse(status_code=400)
+
+    for patient in app.patients:
+        if patient.id == id:
+            return JSONResponse(content=patient.dataInDict(),status_code=200)
+    
+    return JSONResponse(status_code=404)
--- tests.py
+++ tests.py
@@ -12,14 +12,18 @@ client = TestClient(app)
 class structurOfGlobalVar:
     test_register_counter_id = 1
 
+
 gv = structurOfGlobalVar
 
-# test:
+
+# root:
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world!"}
+
+# wykrywanie metody
 
 def test_method():
     response_get = client.get("/method")
@@ -34,7 +38,7 @@ def test_method():
     assert response_put.status_code == 200
 
     assert response_post.json() == {"method": "POST"}
-    assert response_post.status_code == 201 #for post is 201! not 201
+    assert response_post.status_code == 201  # for post is 201! not 201
 
     assert response_options.json() == {"method": "OPTIONS"}
     assert response_options.status_code == 200
@@ -42,9 +46,11 @@ def test_method():
     assert response_delete.json() == {"method": "DELETE"}
     assert response_delete.status_code == 200
 
+# sprawdzania szyforwania
+
 def test_auth_hash():
-    password = ["abcd","hgdaa","hvsu","jhuhujvds"]
-    
+    password = ["abcd", "hgdaa", "hvsu", "jhuhujvds"]
+
     for ele in password:
         sha512_hash = hashlib.sha512()
         sha512_hash.update(bytes(ele, encoding="ASCII"))
@@ -55,31 +61,34 @@ def test_auth_hash():
         assert response.status_code == 401
 
     # test dla zadanych wartsci
-    password="haslo"
-    password_hash="013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215"
+    password = "haslo"
+    password_hash = "013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215"
     sha512_hash = hashlib.sha512()
     sha512_hash.update(bytes(password, encoding="ASCII"))
     assert sha512_hash.hexdigest() == password_hash
 
-    password="haslo"
-    password_hash="f34ad4b3ae1e2cf33092e2abb60dc0444781c15d0e2e9ecdb37e4b14176a0164027b05900e09fa0f61a1882e0b89fbfa5dcfcc9765dd2ca4377e2c794837e091"
+    password = "haslo"
+    password_hash = "f34ad4b3ae1e2cf33092e2abb60dc0444781c15d0e2e9ecdb37e4b14176a0164027b05900e09fa0f61a1882e0b89fbfa5dcfcc9765dd2ca4377e2c794837e091"
     sha512_hash = hashlib.sha512()
     sha512_hash.update(bytes(password, encoding="ASCII"))
     assert sha512_hash.hexdigest() != password_hash
 
-def addDayToDateYMD(date_str:str, days:int, seperator:str = '-'):
+
+
+# sprawdzanie dodawania nowych pacjetow
+
+def addDayToDateYMD(date_str: str, days: int, seperator: str = '-'):
     YMD = [int(ele) for ele in date_str.split(seperator)]
-    date = datetime.date(YMD[0],YMD[1],YMD[2])
+    date = datetime.date(YMD[0], YMD[1], YMD[2])
     date += datetime.timedelta(days=days)
     return date
 
-
 @pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy"])
-@pytest.mark.parametrize("surname",["Nowak","Kowalczyk","Tkacz"])
-def test_register(name: str,surname: str):
-    data = {"name": name,"surname": surname}
-    vaccination_date = addDayToDateYMD("2021-04-01",len(name+surname))
+@pytest.mark.parametrize("surname", ["Nowak", "Kowalczyk", "Tkacz"])
+def test_register(name: str, surname: str):
+    data = {"name": name, "surname": surname}
     tday = datetime.date.today()
+    vaccination_date = addDayToDateYMD(str(tday), len(name + surname))
     respons = {
         "id": gv.test_register_counter_id,
         "name": name,
@@ -87,10 +96,31 @@ def test_register(name: str,surname: str):
         "register_date": str(tday),
         "vaccination_date": str(vaccination_date)
     }
-    
-    gv.test_register_counter_id+=1
+
+    gv.test_register_counter_id += 1
     r = client.post("/register", data=json.dumps(data))
-    
+
     assert r.json() == respons
     assert r.status_code == 201
-    
+
+
+# sprawdzanie odczytwania pacjetow:
+SIZE_OF_APP_PATIENTS = 9
+@pytest.mark.parametrize("id", [index for index in range(1,SIZE_OF_APP_PATIENTS+1)])
+def test_get_patient_id(id: int):
+    r = client.get(f"/patient/{id}")
+    expected_code = 200
+    assert r.status_code == expected_code
+    assert r.json() == app.patients[id-1].dataInDict()
+
+@pytest.mark.parametrize("id", [index for index in range(SIZE_OF_APP_PATIENTS+2,20)])
+def test_get_patient_id(id: int):
+    r = client.get(f"/patient/{id}")
+    expected_code = 404
+    assert r.status_code == expected_code
+
+def test_get_patient_id():
+    id = -1
+    r = client.get(f"/patient/{id}")
+    expected_code = 400
+    assert r.status_code == expected_code
